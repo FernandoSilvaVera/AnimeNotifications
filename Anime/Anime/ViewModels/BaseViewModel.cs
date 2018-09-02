cae7322b3b4c5ce60c0b2cd94cfaed6cb795b61d
ShuviDola <fernandosilv4c@gmail.com>
2018-09-02 11:53:18 +0200

    clean fields

---
+++ Anime/Anime/ViewModels/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Anime.ViewModels
+{
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Runtime.CompilerServices;
+
+	public class BaseViewModel : INotifyPropertyChanged
+    {
+		#region Events
+		public event PropertyChangedEventHandler PropertyChanged;
+		#endregion
+
+		#region Methods
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (EqualityComparer<T>.Default.Equals(backingField, value))
+			{
+				return;
+			}
+			backingField = value;
+			OnPropertyChanged(propertyName);
+		}
+		#endregion
+
+	}
+}
