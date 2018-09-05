3150c6f58d527a99cad45fdcfa3d14307fc774fb
ShuviDola <fernandosil4c@gmail.com>
2018-09-05 20:15:59 +0200

    MainViewModel

--- Anime/Anime/ViewModels/MainViewModel.cs
+++ Anime/Anime/ViewModels/MainViewModel.cs
@@ -2,20 +2,25 @@
 {
     class MainViewModel
     {
-
 		#region ViewModels
 		public LoginViewModel Login { get; set; }
+		public ListaAnimesViewModel ListaAnimes { get; set; }
 		#endregion
 
 		#region Constructors
 		public MainViewModel()
 		{
+			instance = this;
 			Login = new LoginViewModel();
 		}
 		#endregion
 
 		#region Methods
-
+		#endregion
+		
+		#region Singleton
+		private static MainViewModel instance;
+		public static MainViewModel Instance => instance is null ? new MainViewModel() : instance;
 		#endregion
 	}
 }
