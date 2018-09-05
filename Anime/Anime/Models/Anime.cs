653f4e3ac4f611509d8642cb92416039217ab98a
ShuviDola <fernandosil4c@gmail.com>
2018-09-05 20:16:30 +0200

    Model Anime

---
+++ Anime/Anime/Models/Anime.cs
@@ -0,0 +1,11 @@
+﻿namespace Anime.Models
+{
+    class Anime
+    {
+		public string Nombre { get; set; }
+
+		public string Capitulos { get; set; }
+
+		public string Descripcion { get; set; }
+	}
+}
