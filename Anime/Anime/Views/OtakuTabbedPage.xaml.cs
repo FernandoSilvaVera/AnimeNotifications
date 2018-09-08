e4f6e6d6c07f675b63666f69bf95435c20f449fd
ShuviDola <fernandosil4c@gmail.com>
2018-09-08 13:16:39 +0200

    Tabbed Page

---
+++ Anime/Anime/Views/OtakuTabbedPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Anime.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Anime.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OtakuTabbedPage : TabbedPage
+	{
+		public OtakuTabbedPage ()
+		{
+			MainViewModel.Instance.ListaAnimes = new ListaAnimesViewModel();
+			InitializeComponent ();
+		}
+	}
+}
