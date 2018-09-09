404e9fa487ec6188e226ff90d0ebd8451fa3f4cc
ShuviDola <fernandosil4c@gmail.com>
2018-09-09 20:20:04 +0200

    Chapter

---
+++ Anime/Anime/Models/Chapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anime.Models
+{
+    public class Chapter
+    {
+		#region Properties
+		public int Number { get; set; }
+		#endregion
+		public Chapter(int number) => Number = number;
+    }
+}
